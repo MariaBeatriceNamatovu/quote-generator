179195c815150dba45bfb1660fbe788a10464176
MariaBeatriceNamatovu <namatovutrismari@gmail.com>
2024-01-25 16:39:37 +0300

    quote generator

--- assignment.js
+++ assignment.js
@@ -1,22 +1,21 @@
 const quoteResult = document.getElementById('result');
-
-function getRandomQuote() {
-    fetch('https://api.quotable.io/random')
-        .then(response => response.json())
-        .then(data => {
-            const quote = data.content;
-            quoteResult.innerHTML = `<p>${quote}</p>`; 
-            
-        })
-        .catch(error => {
-            console.log(error);
-        });
+ 
+async function getRandomQuote() {
+    try {
+        const response = await fetch('https://api.quotable.io/random');
+ 
+        if (!response.ok) {
+            throw new Error(`Network response was not ok: ${response.status}`);
+        }
+ 
+        const data = await response.json();
+        const quote = data.content;
+ 
+        quoteResult.innerHTML = `<p>${quote}</p>`;
+    } catch (error) {
+        console.error(error);
+    }
 }
-
+ 
 const generatorButton = document.getElementById('generate-button');
 generatorButton.addEventListener('click', getRandomQuote);
-
-
-// function displayQuote(){
-//    
-// }
